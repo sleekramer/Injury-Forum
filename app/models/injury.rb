c840e23e196882d115086cc53419b24dc7b864c3
Steven Lee-Kramer <sleekramer@gmail.com>
2015-12-18 11:21:38 -0500

    Fix errors in injuries controller and model

--- app/models/injury.rb
+++ app/models/injury.rb
@@ -3,5 +3,5 @@ class Injury < ActiveRecord::Base
   belongs_to :body_part
 
   validates :name, presence: true, length: {minimum: 10}, uniqueness: true
-  validates :name, presence: true, length: {minimum: 30}
+  validates :description, presence: true, length: {minimum: 30}
 end
