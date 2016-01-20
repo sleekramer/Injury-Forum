2e240f13a1912a2b88a764842990394b4adbea53
Steven Lee-Kramer <sleekramer@gmail.com>
2016-01-20 00:43:48 -0500

    Update seeds.rb to generate all the symptoms

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+include GlobalVariables
+
 body_parts = ["Foot & Heel", "Ankle", "Lower Leg", "Knee", "Thigh", "Hip & Groin", "Abdomen", "Lower Back", "Upper Back", "Chest", "Shoulder", "Arm", "Elbow", "Wrist", "Hand", "Neck", "Head & Face", "General Conditions" ]
 
 18.times do |i|
@@ -6,5 +8,12 @@ body_parts = ["Foot & Heel", "Ankle", "Lower Leg", "Knee", "Thigh", "Hip & Groin
   end
 end
 
+GlobalVariables::SYMPTOMS.each do |symptom|
+  unless Symptom.where(name: symptom).count > 0
+    Symptom.create!(name: symptom)
+  end
+end
+
 puts "Finished."
 puts "#{BodyPart.count} body parts in database"
+puts "#{Symptom.count} symptoms in database"
